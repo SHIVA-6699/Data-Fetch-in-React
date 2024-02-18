19953d3f9ae2f971d3ca296ac4808b02deac82b0
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-02-18 17:16:45 +0530

    Fetch Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState,useRef } from 'react';
 import './App.css';
-
+interface Post{
+  id:string,
+  title:string
+}
+const BASE_URL="https://jsonplaceholder.typicode.com"
 function App() {
+  const abortControllerRef = useRef<AbortController | null>(null);
+  const [data,SetData]=useState<Post[]>();
+  const [error,Seterror]=useState<boolean>();
+  const[loading,SetLoading]=useState<boolean>(false)
+  useEffect(()=>{
+      abortControllerRef.current?.abort(); 
+      abortControllerRef.current = new AbortController(); // it is abort the previous API call when spam click
+    async function getData(){
+      SetLoading(true)
+      try{
+        
+        const response=await fetch(`${BASE_URL}/posts`,{
+          signal:abortControllerRef.current?.signal,
+        })
+        const posts=(await response.json()) as Post[];
+        SetData(posts)
+    }
+    catch(e:any){
+        if (e.name === "AbortError") {
+          console.log("Aborted");
+          return;
+        }
+      Seterror(e)
+    }
+    finally{
+      
+      SetLoading(false)
+    }
+  }
+  getData()
+},[])
+if(error){
+  return <div>Something Went Error</div>
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {loading && <p>Loading....</p>}
+    {data?.map((Post)=>{
+      return <div key={Post.id}> and {Post.title}</div>
+    })}
     </div>
   );
 }
